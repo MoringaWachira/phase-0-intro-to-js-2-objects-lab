782915b10ec9fb3dc40640ffb151b405a9250e4c
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-20 19:39:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Silvia",
+    streetAddress: "200 Joyland",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeInfo = {...employee, [key]: value}
+    return employeeInfo;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;  
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+    const newEmployee = {...employee}
+    delete newEmployee [key]
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee [key]
+    return employee;
+}
